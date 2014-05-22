68811a10b662ad7b889cb34ca3e9630e779a159f
Praktikan <Praktikan@604-02>
2014-05-22 16:10:43 +0700

    bebas

---
+++ src/test/java/TestClass.java
@@ -0,0 +1,13 @@
+import org.junit Before;
+import org.junit Test;
+import org.junit After;
+import static org.junit Assert.*;
+
+public class TestClass {
+    private static Soal S;
+    
+    @Before
+    S = new Soal();
+    
+    
+}
